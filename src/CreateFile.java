be716772549151703e76f1e4aec63b9a65d074a1
malekcoders <alomayri970@outlook.com>
2023-11-26 16:54:05 +0300

    rm src

--- src/CreateFile.java
+++ src/CreateFile.java
@@ -7,7 +7,6 @@ public class CreateFile {
 
     public static void main(String[] args) throws IOException{
         /* You Code Here */
-        Path path = Paths.get("src/notenote.txt");
-        Files.createFile(path);
+        
     }
 }
